cf2b0838d8c5e718809da3a67e112d1374722aad
Taiwo Triumphant <taiwotriumphant@gmail.com>
2025-10-24 18:49:19 +0100

    something fixed

--- smart-contracts/contracts/IPNFTMarketplace.sol
+++ smart-contracts/contracts/IPNFTMarketplace.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
